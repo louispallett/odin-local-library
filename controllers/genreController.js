aa980be658111c6ae169d926e2cf94ed701aedf9
Louis Pallett <louis.pallett@outlook.com>
2024-04-01 02:07:33 +0100

    Implemented delete and update get and post requests

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,4 +1,5 @@
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 const Book = require("../models/book");
 const Genre = require("../models/genre");
 
@@ -33,14 +34,54 @@ exports.genre_detail = asyncHandler(async (req, res, next) => {
 });
 
 // Display Genre create form on GET.
-exports.genre_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre create GET");
-});
+// Note, we don't require the asyncHandler wrapper here because it doesn't contain any code which can throw an exception!
+exports.genre_create_get = (req, res, next) => {
+  res.render("genre_form", { title: "Create Genre" });
+};
 
 // Handle Genre create on POST.
-exports.genre_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre create POST");
-});
+exports.genre_create_post = [
+  // Validate and sanitize the name field.
+  body("name", "Genre name must contain at least 3 characters")
+    .trim() // Removes any whitespace
+    .isLength({ min: 3 }) // Checks length
+    .escape(), // Removes any dangerous HTML characters (docs: https://express-validator.github.io/docs/6.6.0/sanitization/)
+
+  // Process request after validation and sanitization.
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors from a request (stores in an object
+    // see docs: https://express-validator.github.io/docs/6.0.0/validation-result-api/)
+    const errors = validationResult(req);
+
+    // Create a genre object with escaped and trimmed data.
+    const genre = new Genre({ name: req.body.name });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.render("genre_form", {
+        title: "Create Genre",
+        genre: genre,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid.
+      // Check if Genre with same name already exists.
+      const genreExists = await Genre.findOne({ name: req.body.name })
+      .collation({ locale: "en", strength: 2 })
+      .exec();
+      if (genreExists) {
+        // Genre exists, redirect to its detail page.
+        res.redirect(genreExists.url);
+      } else {
+        await genre.save();
+        // New genre saved. Redirect to genre detail page.
+        res.redirect(genre.url);
+      }
+    }
+  }),
+];
+
 
 // Display Genre delete form on GET.
 exports.genre_delete_get = asyncHandler(async (req, res, next) => {
--- models/bookinstance.js
+++ models/bookinstance.js
@@ -23,6 +23,10 @@ BookInstanceSchema.virtual("url").get(function () {
 // Formatting date and time (using luxon library)
 BookInstanceSchema.virtual("due_back_formatted").get(function () {
     return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
+});
+
+BookInstanceSchema.virtual("due_back_yyyy_mm_dd").get(function () {
+    return DateTime.fromJSDate(this.due_back).toISODate(); // format 'YYYY-MM-DD'
 });  
 
 module.exports = mongoose.model("BookInstance", BookInstanceSchema);
