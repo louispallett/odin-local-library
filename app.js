09a0e58f56c5f9eed6360aa64e482c509bd94335
Louis Pallett <louis.pallett@outlook.com>
2024-04-02 09:41:49 +0100

    Added security modules

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const compression = require("compression");
+const helmet = require("helmet");
 
 const connectionString = require("./public/javascripts/connection");
 
@@ -11,9 +13,18 @@ const catalogRouter = require("./routes/catalog");
 
 const app = express();
 
+// Set up rate limiter: maximum of twenty requests per minute (this stops brute force attacks)
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+app.use(limiter)
+
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = connectionString;
+const dev_db_url = connectionString;
+const mongoDB = process.env.MONGODB_URI || dev_db_url
 
 main().catch((err) => console.log(err));
 async function main() {
@@ -25,10 +36,23 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // Various dependencies
+app.use(compression()); // For compressed server errors (see https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/deployment)
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Critical for security. It sets HTTP headers to protect from (well-known) web vulnerabilities.
+// See https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/deployment
+// In the template pug folder we included some bootstrap and jQuery scripts, so we have to pass these in (note that normally we would
+// have these as dependencies, so this wouldn't be a problem, so we have to mark this up here. This shouldn't be done normally as it 
+// goes against helmet's policy of external scripts).
+// More on Helmet: https://www.npmjs.com/package/helmet
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+  },
+}));
 
 // Set public/ to a static directory
 app.use(express.static(path.join(__dirname, 'public')));
